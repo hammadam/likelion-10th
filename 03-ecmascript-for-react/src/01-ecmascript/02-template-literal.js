7ebb834ea4511308aa01c16d185d87736b1be0b3
hammadam <hammadam@naver.com>
2024-07-22 16:38:38 +0900

    수업내용 첫 업로드

---
+++ 03-ecmascript-for-react/src/01-ecmascript/02-template-literal.js
@@ -0,0 +1,71 @@
+// --------------------------------------------------------------------------
+// Template literal
+// - 데이터 + 템플릿(js) = 마크업 스트링
+// - 템플릿 리터럴 구문을 사용해 마크업 스트링 생성
+// --------------------------------------------------------------------------
+
+// 데이터(상태 : status)
+// 클라이언트 요청 => 서버 -> 클라이언트 응답 -> JSON
+const koreanFoods = {
+  caption: "한식 메뉴",
+  rows: [
+    { headline: "뚝배기 불고기", content: 8000 },
+    { headline: "스팸치즈볶음밥", content: 7500 },
+    { headline: "불고기낙지덮밥", content: 9000 },
+  ],
+};
+
+function renderTable(data) {
+  return [
+    '<table class="table">',
+    '<caption class="sr-only">' + data.caption + "</caption>",
+    data.rows.reduce(function (htmlString, rowData) {
+      const rowString = [
+        "<tr>",
+        "<th>" + rowData.headline + "</th>",
+        "<td>" + numberWithComma(rowData.content) + "원" + "</td>",
+        "</tr>",
+      ].join("");
+      return htmlString + rowString;
+    }, ""),
+    "</table>",
+  ].join("");
+}
+
+// 🔶 renderTableString 함수를 작성하세요.
+function renderTableString (data /* {caption : string, rows:[] */}){
+  return /* html */`
+  <table class="tabel">
+  <caption class="sr-only">${data.caption}</caption>
+  ${data.rows.reduce(function(htmlString, rowItem){
+    return /* htmnl */`
+      <tr>
+        <th>${rowItem.headline}</th>
+        <td>${rowItem.content}원</td>
+      </tr>
+    `;
+  }, '')}
+  </table>
+  `;
+}
+
+
+function run() {
+  const renderedResult = renderTable(koreanFoods);
+  return renderedResult;
+}
+
+console.log(run());
+
+// --------------------------------------------------------------------------
+// utils
+
+function numberWithComma(numberValue) {
+  return numberValue.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+}
+
+function removeSpaceHTMLString(htmlString) {
+  return htmlString.replace(/\s+<|\n|>\s+/g, function ($1) {
+    return $1.indexOf("<") > -1 ? "<" : $1.indexOf(">") > -1 ? ">" : "";
+  });
+}
